9f943b88a186caf650c64e580bda7d2468f9637a
Rune Holmgren <Raane.Holm@gmail.com>
2014-01-14 21:16:45 +0100

    Add fragments handling each main menu

---
+++ src/com/rate_me/RateFragment.java
@@ -0,0 +1,30 @@
+package com.rate_me;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class RateFragment extends Fragment{
+
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+
+	public RateFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_main_dummy,
+				container, false);
+		TextView dummyTextView = (TextView) rootView
+				.findViewById(R.id.section_label);
+		dummyTextView.setText("Rate");
+		return rootView;
+	}
+}
